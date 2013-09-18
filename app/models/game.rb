6e0ee746bdd086c737853b9ff61c5935ac144d25
Phillip Boksz <phillip.boksz@llp.pl>
2013-09-18 11:55:37 +0200

    Add dependant destroy to has_many relation

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ActiveRecord::Base
   attr_accessible :name
 
-  has_many :items
+  has_many :items, :dependent => :destroy
 
   def query_name
     name.downcase.gsub(' ', '+')
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
   attr_accessible :name, :link_url, :image_url, :foil, :currency_symbol, :game
 
-  has_many :daily_stats
+  has_many :daily_stats, :dependent => :destroy
   belongs_to :game
 
   def short_name
