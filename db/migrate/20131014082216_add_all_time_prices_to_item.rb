657b6f83024e9506fb76cddfff8c8f97b8deb50a
Phillip Boksz <phillip.boksz@llp.pl>
2013-10-19 18:58:10 +0200

    Remove active record migrations and schema

--- db/migrate/20131014082216_add_all_time_prices_to_item.rb
+++
@@ -1,6 +0,0 @@
-class AddAllTimePricesToItem < ActiveRecord::Migration
-  def change
-    add_column :items, :all_time_high_price_integer, :integer, :default => 0
-    add_column :items, :all_time_low_price_integer, :integer, :default => 0
-  end
-end
