6345f357d7265917d56cd2b2744738d8f581f42e
Phillip Boksz <phillip.boksz@llp.pl>
2013-10-19 16:16:49 +0200

    Remove active admin since it does not work with mongoid

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ SteamCardTracker::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-
   root 'home#index'
   resources :games, :only => [:index, :show]
 end
