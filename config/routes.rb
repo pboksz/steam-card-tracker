1cf0c379d4d9f32429906d41e99a14b4e83e9ab4
Phillip Boksz <phillip@lunarlogic.io>
2015-03-03 12:29:34 +0100

    Use specific api routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ SteamCardTracker::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  namespace :api do
+    resources :games, only: [:index, :show]
+  end
+
+  resources :games, only: [:index, :create]
+
   root 'home#index'
-  resources :games, :only => [:index, :show, :new, :create]
 end
