f17498d5d439f24c7160771bd5eab528c2253e89
Phillip Boksz <phillip@lunarlogic.io>
2015-03-03 12:26:44 +0100

    Add default repository

---
+++ app/repositories/default_repository.rb
@@ -0,0 +1,19 @@
+class DefaultRepository
+  attr_reader :klass
+
+  def initialize(klass)
+    @klass = klass
+  end
+
+  def all
+    klass.all
+  end
+
+  def new(attributes = {})
+    klass.new(attributes)
+  end
+
+  def create(attributes = {})
+    klass.create(attributes)
+  end
+end
