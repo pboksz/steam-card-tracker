a3c8603409c9738607f8baf80ee559e1c70dc4b0
Phillip Boksz <phillip@lunarlogic.io>
2015-03-11 15:10:03 +0100

    Add games controller specs

---
+++ spec/controllers/games_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe GamesController do
+  let(:repository) { double }
+  before { allow(DefaultRepository).to receive(:new).with(Game).and_return(repository) }
+
+  describe 'GET #index' do
+    let(:game) { double }
+    let(:games) { double }
+    before do
+      expect(repository).to receive(:new).and_return(game)
+      expect(repository).to receive(:all).and_return(games)
+      get :index
+    end
+
+    it { expect(response).to render_template :index, locals: { game: game, games: games } }
+  end
+
+  describe 'POST #create' do
+    let(:params) { { name: 'Game Name' } }
+    before do
+      expect(repository).to receive(:create).with(params)
+      post :create, :game => params
+    end
+
+    it { expect(response).to redirect_to games_path }
+  end
+end
