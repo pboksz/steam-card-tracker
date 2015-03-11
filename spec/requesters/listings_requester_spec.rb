d7baa05ee9e29ea3ecc1ff903e31f40e76a8608b
Phillip Boksz <phillip@lunarlogic.io>
2015-03-11 16:00:07 +0100

    Fix listings requester test coverage

--- spec/requesters/listings_requester_spec.rb
+++ spec/requesters/listings_requester_spec.rb
@@ -5,14 +5,12 @@ describe ListingsRequester do
   let(:listing_requester) { ListingsRequester.new(request) }
 
   describe '#response' do
-    let(:parser) { double }
     let(:body) { double }
     let(:requester) { double(perform: double(body: body)) }
     let(:response) { double }
     before do
-      allow(listing_requester).to receive(:parser).and_return(parser)
-      allow(listing_requester).to receive(:requester).and_return(requester)
-      expect(parser).to receive(:parse).with(body).and_return(response)
+      allow(Weary::Request).to receive(:new).with(request).and_return(requester)
+      expect(JSON).to receive(:parse).with(body).and_return(response)
     end
 
     it { expect(listing_requester.response).to eq response }
