066e0e08dd6dbea93e8616f359756955d8509876
Phillip Boksz <phillip@lunarlogic.io>
2015-03-12 14:05:54 +0100

    Use listing in game parser

--- app/parsers/game_parser.rb
+++ app/parsers/game_parser.rb
@@ -17,10 +17,6 @@ class GameParser
   end
 
   def is_regular_item_from_correct_game?
-    !!(parse_game_name =~ /^(#{Regexp.escape(game.name)}) (trading card)$/i)
-  end
-
-  def parse_game_name
-    listing.css('.market_listing_row .market_listing_game_name').first.content
+    !!(listing.game_name =~ /^(#{Regexp.escape(game.name)}) (trading card)$/i)
   end
 end
--- spec/parsers/game_parser_spec.rb
+++ spec/parsers/game_parser_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 describe GameParser do
   let(:game) { build(:game) }
-  let(:listing) { TestListing.new }
   let(:game_parser) { GameParser.new(game, listing) }
 
   describe '#parse' do
     describe 'game name not correct' do
+      let(:listing) { double(game_name: "Invalid") }
       it { expect(game_parser.parse).to be_nil }
     end
 
     describe 'when is regular item from game' do
-      let(:game) { build(:game, name: 'Game') }
+      let(:listing) { double(game_name: "#{game.name} Trading Card") }
       let(:item_parser) { double }
       before do
         allow(ItemParser).to receive(:new).with(game.items, listing).and_return(item_parser)
