1289b54721359ada3cc357236408c4b7f86f3c3c
Phillip Boksz <phillip@lunarlogic.io>
2015-03-12 14:09:05 +0100

    Use listing parser in stat parser

--- app/parsers/stat_parser.rb
+++ app/parsers/stat_parser.rb
@@ -7,16 +7,12 @@ class StatParser
   end
 
   def parse
-    stats_repository.update_prices_for_today(parse_price)
+    stats_repository.update_prices_for_today(listing.price)
   end
 
   private
 
   def stats_repository
     @stats_repository ||= StatsRepository.new(stats)
-  end
-
-  def parse_price
-    listing.css('.market_listing_row .market_table_value span').first.content.match(/\d+.\d{1,2}/).to_s.to_f
   end
 end
--- spec/parsers/stats_parser_spec.rb
+++ spec/parsers/stats_parser_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe StatParser do
   let(:stat) { build(:stat) }
   let(:stats) { [stat] }
-  let(:listing) { TestListing.new }
+  let(:listing) { double(price: 0.1) }
   let(:stat_parser) { StatParser.new(stats, listing) }
 
   describe '#parse' do
@@ -11,7 +11,7 @@ describe StatParser do
 
     before do
       allow(StatsRepository).to receive(:new).with(stats).and_return(repository)
-      expect(repository).to receive(:update_prices_for_today).with(0.1)
+      expect(repository).to receive(:update_prices_for_today).with(listing.price)
     end
 
     it { stat_parser.parse }
