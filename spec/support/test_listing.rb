4b6f6cb32258f8e39acac72af05d8d3280d1304d
Phillip Boksz <phillip@lunarlogic.io>
2015-03-12 13:17:33 +0100

    Add test listing to support specs

---
+++ spec/support/test_listing.rb
@@ -0,0 +1,22 @@
+class TestListing
+  def self.new
+    Nokogiri::HTML(html)
+  end
+
+  private
+
+  def self.html
+    <<-html
+      <a class="market_listing_row_link" href="/link">
+        <div class="market_listing_row">
+          <div class="market_listing_game_name">Game Trading Card</div>
+          <div class="market_listing_item_name">Trading Card</div>
+          <img src="image.jpg">
+          <div class="market_table_value">
+            <span>0.10</span>
+          </div>
+        </div>
+      </a>
+    html
+  end
+end
