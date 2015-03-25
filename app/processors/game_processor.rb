beb7187b6a0b4501186ab7696f122763cb5d1e42
Phillip Boksz <phillip@lunarlogic.io>
2015-03-25 13:29:17 +0100

    Also update game after items are processed

--- app/processors/game_processor.rb
+++ app/processors/game_processor.rb
@@ -7,16 +7,23 @@ class GameProcessor
   end
 
   def process
-    item_processor.process if is_regular_item_from_correct_game?
+    if is_from_correct_game?
+      item_processor.process
+      games_repository.update(game.id)
+    end
   end
 
   private
+
+  def games_repository
+    @games_repository ||= GamesRepository.new(Game)
+  end
 
   def item_processor
     @item_processor ||= ItemProcessor.new(game.items, listing)
   end
 
-  def is_regular_item_from_correct_game?
+  def is_from_correct_game?
     !!(listing.game_name =~ /^(#{Regexp.escape(game.name)}) (trading card)$/i)
   end
 end
