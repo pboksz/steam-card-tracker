9c42fb06def1d46094cddffcacc7e198113b7919
Phillip Boksz <phillip@lunarlogic.io>
2015-03-25 15:42:44 +0100

    Do not update game in game processor

--- app/processors/game_processor.rb
+++ app/processors/game_processor.rb
@@ -7,23 +7,16 @@ class GameProcessor
   end
 
   def process
-    if is_from_correct_game?
-      item_processor.process
-      games_repository.update(game.id)
-    end
+    item_processor.process if is_from_correct_game?
   end
 
   private
-
-  def games_repository
-    @games_repository ||= GamesRepository.new(Game)
-  end
 
   def item_processor
     @item_processor ||= ItemProcessor.new(game.items, listing)
   end
 
   def is_from_correct_game?
-    !!(listing.game_name =~ /^(#{Regexp.escape(game.name)}) (trading card)$/i)
+    !!(listing.game_name =~ /^(#{Regexp.escape(game.name)})\s+(trading card)$/i)
   end
 end
--- spec/processors/game_processor_spec.rb
+++ spec/processors/game_processor_spec.rb
@@ -10,15 +10,12 @@ describe GameProcessor do
       it { expect(game_processor.process).to be_nil }
     end
 
-    describe 'when is regular item from game' do
+    describe 'game name is correct' do
       let(:listing) { double(game_name: "#{game.name} Trading Card") }
       let(:item_processor) { double }
-      let(:games_repository) { double }
       before do
         allow(ItemProcessor).to receive(:new).with(game.items, listing).and_return(item_processor)
-        allow(GamesRepository).to receive(:new).with(Game).and_return(games_repository)
         expect(item_processor).to receive(:process)
-        expect(games_repository).to receive(:update).with(game.id)
       end
 
       it { game_processor.process }
