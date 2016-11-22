d0f2939429035fa1b13598fa52726c8992598242
Phillip Boksz <pboksz@gmail.com>
2016-11-22 21:12:04 +0000

    Fix ruby 1.8 syntax in games repository

--- app/repositories/games_repository.rb
+++ app/repositories/games_repository.rb
@@ -1,7 +1,7 @@
 class GamesRepository < DefaultRepository
   def update(id)
     game = find(id: id)
-    game.update_attributes(price_per_badge: game.items.sum(&:latest_price), :updated_at => Time.now)
+    game.update_attributes(price_per_badge: game.items.sum(&:latest_price), updated_at: Time.now)
 
     game
   end
