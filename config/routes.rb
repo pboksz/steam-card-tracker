47a425d79c64dbc7883df49d2591bdb0a4613dc3
Phillip Boksz <pboksz@gmail.com>
2018-04-13 10:16:00 +0100

    Add game show and items destroy controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ SteamCardTracker::Application.routes.draw do
   admin_auth_routes
 
   namespace :admin do
-    resources :games, only: [:index, :create, :destroy]
+    resources :games, only: [:index, :create, :show, :destroy] do
+      resources :items, only: [:destroy]
+    end
   end
 
   namespace :api do
